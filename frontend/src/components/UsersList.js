96d200f14b988682c5c9d3ff041728de7cfc34c3
fanguyen <ngu.fabien@gmail.com>
2022-06-16 18:07:57 -0400

    add UsersList component, users api call

---
+++ frontend/src/components/UsersList.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const UserListContainer = styled.div`
+    margin-left: 10px;
+`
+
+const UserCard = styled.div`
+    background-color: rgb(4, 4, 63);
+    margin: 5px 0;
+    padding: 10px;
+    border: 1px solid;
+    border-radius: 8px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    max-width: 300px;
+`
+
+const UserInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-left: 10px
+`
+
+const UserName = styled.div`
+    color: #ffffff;
+    margin-bottom: 5px;
+`
+
+const UserPosition = styled.div`
+    color: #ffffff;
+`
+
+const UserAvatar = styled.img`
+    color: #ffffff;
+    border: 1px solid;
+    border-radius: 50px;
+`
+
+const Title = styled.h3`
+    color: rgb(4, 4, 63);
+`
+
+function UsersList({ users }) {
+    return (
+        <UserListContainer>
+            <Title>Users List</Title>
+            {users.map(user => (
+                <UserCard key={user.id}>
+                    <UserAvatar src={user.avatar_url} />
+                    <UserInfo>
+                        <UserName>{user.name}</UserName>
+                        <UserPosition>{user.position}</UserPosition>
+                    </UserInfo>
+                </UserCard>
+            ))}
+        </UserListContainer>
+    )
+}
+
+export default UsersList;
