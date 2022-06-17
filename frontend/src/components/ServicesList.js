da6cb03b36142188e89d39a0549f36701d89ee8f
fanguyen <ngu.fabien@gmail.com>
2022-06-17 10:28:05 -0400

    add cost price and some styling

--- frontend/src/components/ServicesList.js
+++ frontend/src/components/ServicesList.js
@@ -5,15 +5,15 @@ const LinkButton = styled.a`
     color: #000000;
     height: 60px;
     width: 60px;
-    margin-right: 30px;
     display: flex;
+    align-items: center;
     border: 1px solid transparent;
     border-radius: 50px;
     background-color: #ffffff
 `
 
 const Title = styled.h3`
-    color: rgb(4, 4, 63);
+    color: #000000;
 `
 
 const Logo = styled.img`
@@ -33,15 +33,47 @@ const ServicesBox = styled.div`
     flex-direction: row
 `
 
-function ServicesList({ services, onSelectService }) {
+const Service = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: #ffffff;
+    margin: 5px 0;
+    padding: 10px;
+    border: 3px solid #ffffff;
+    border-radius: 8px;
+    margin-right: 20px;
+    text-decoration: none;
+    cursor: pointer
+`
+
+const ServicePrice = styled.div`
+    color: #333;
+    margin-top: 20px;
+`
+
+function ServicesList({ services, onSelectService, users, activeService }) {
+    const serviceMonthlyCost = (servicePrice) => {
+        const monthlyCost = servicePrice.flat_cost + servicePrice.cost_per_user * (users.length - servicePrice.nb_users_included)
+        return monthlyCost / 100
+    }
+
     return (
         <ServicesContainer>
             <Title>Services List</Title>
             <ServicesBox>
                 {services.map(service => (
-                    <LinkButton key={service.id} href={service.website_url} target="_blank" onClick={() => onSelectService(service.id)}>
-                        <Logo src={service.logo_url}/>
-                    </LinkButton>
+                    <Service
+                        key={service.id}
+                        onClick={() => onSelectService(service.id)}
+                        style={service.id === activeService ? { borderColor: `rgba(83, 0, 236, 0.96)`} : {}}
+                    >
+                        <LinkButton href={service.website_url} target="_blank">
+                            <Logo src={service.logo_url}/>
+                        </LinkButton>
+                        <ServicePrice>Cost: {serviceMonthlyCost(service.price)}</ServicePrice>
+                    </Service>
                 ))}
             </ServicesBox>
         </ServicesContainer>
